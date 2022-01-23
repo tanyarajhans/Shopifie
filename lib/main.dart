e734040a4ad931ad02fa79a4e2adc9d023c3a06b
Tanya <tanyarajhans7@gmail.com>
2022-01-23 21:46:35 +0530

    Added Bottombar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopifie/screens/bottom_bar.dart';
 import 'package:shopifie/screens/home.dart';
 
 void main() {
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Home(),
+      home: BottomBar(),
     );
   }
 }
--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -22,10 +22,79 @@ class _BottomBarState extends State<BottomBar> {
     Cart(),
     UserInfo()
   ];
+
+  int _index=0;
+
+  void _selectedPage(int index){
+    setState(() {
+      _index = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: _pages[_index],
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 0.01,
+        clipBehavior: Clip.antiAlias,
+        child: Container(
+          height: kBottomNavigationBarHeight,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border(
+                top: BorderSide(
+                  color: Colors.grey,
+                  width: 0.5,
+                ),
+              ),
+            ),
+            child:BottomNavigationBar(
+          onTap: _selectedPage,
+          backgroundColor: Theme.of(context).primaryColor,
+          unselectedItemColor: Theme.of(context).textSelectionColor,
+          selectedItemColor: Colors.purple,
+          currentIndex: _index,
+          items: [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  // title: Text('Home'),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.rss_feed), label: 'Feeds'),
+                BottomNavigationBarItem(
+                    activeIcon: null, icon: Icon(null), label: 'Search'),
+                BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.shopping_bag,
+                    ),
+                    label: 'Cart'),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.person), label: 'User'),
+              ],
+        ),
+      ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniCenterDocked,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FloatingActionButton(
+        backgroundColor: Colors.purple,
+          hoverElevation: 10,
+          splashColor: Colors.grey,
+          tooltip: 'Search',
+          elevation: 4,
+          child: Icon(Icons.search),
+        onPressed: (){
+          setState(() {
+            _index =2;
+          });
+        },
+      )),
     );
   }
 }
