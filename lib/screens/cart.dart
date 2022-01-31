a8c40eb75b291e499bbb66613d60a981e80ef500
Tanya <tanyarajhans7@gmail.com>
2022-01-31 21:35:22 +0530

    Cart item UI done

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:shopifie/screens/cart_empty.dart';
+import 'package:shopifie/screens/cart_full.dart';
 
 class Cart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CartEmpty()
+      body: CartFull()
       
     );
   }
--- lib/screens/cart_full.dart
+++ lib/screens/cart_full.dart
@@ -11,9 +11,54 @@ class _CartFullState extends State<CartFull> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 130,
+      margin: EdgeInsets.all(30.0),
       decoration: BoxDecoration(
-        color: 
+        borderRadius: BorderRadius.only(bottomRight: Radius.circular(16.0),
+        topRight: Radius.circular(16.0),
+        
+        ),
+        color: Theme.of(context).backgroundColor,
       ),
+      
+      child: Row(
+        children: [
+          Container(
+            width: 130,
+            decoration: BoxDecoration(
+              image: DecorationImage(image: NetworkImage('https://pbs.twimg.com/profile_images/1283958620359516160/p7zz5dxZ.jpg'),
+              fit: BoxFit.fill
+              )
+            ),),
+            Column(
+              
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Module', style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 15.0
+                    )),
+                    SizedBox(width:130),
+                    Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        onTap:(){},
+                        borderRadius: BorderRadius.circular(32.0),
+                        child: Container(
+                          height: 50,
+                          width: 50,
+                          child: Icon(Icons.close, color: Colors.red)
+                        )
+                      ),
+                    )
+                  ],
+                )
+              ],
+            )
+      ],
+      )
     );
   }
 }
