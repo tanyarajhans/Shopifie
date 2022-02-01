a29e141ad96d8682ff87eb338c2f9c43be2e9b52
Tanya <tanyarajhans7@gmail.com>
2022-02-01 19:55:00 +0530

    Bottomsheet added

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -7,9 +7,79 @@ class Cart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List products = [];
-    return Scaffold(
-      body: products.isEmpty? CartEmpty(): CartFull()
+    return !products.isEmpty? Scaffold(
+      body:  CartEmpty()
+    ) : Scaffold(
+      bottomSheet: CheckOutSection(),
+      appBar: AppBar(
+        
+        title: const Text('Cart Itmes Count:', textAlign: TextAlign.left,),
+        actions: [IconButton(onPressed: (){}, 
+        icon: Icon(
+          Icons.delete))],
+      ),
+      body: CartFull(),
       
     );
   }
+
+  
+
 }
+
+class CheckOutSection extends StatelessWidget {
+  
+    @override
+    Widget build(BuildContext context) {
+      return Container(
+        decoration: BoxDecoration(
+        border: Border(top: BorderSide(color: Colors.grey, width: 0.5))
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+          Expanded(
+            flex: 2,
+            child: Material(
+              borderRadius: BorderRadius.circular(30.0),
+              color: Colors.red,
+              child: InkWell(
+                onTap: (){},
+                borderRadius: BorderRadius.circular(30.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Checkout', 
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Theme.of(context).textSelectionColor,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w600,
+                    
+                  )),
+                )
+              )
+            ),
+          ),
+          Spacer(),
+          Text('Total: ', 
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Theme.of(context).textSelectionColor,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w600,
+                    
+                  )),
+                  Text('US \$170.80', 
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w500,
+                    
+                  )),
+        ]),
+      ),
+      );
+    }
+  }
