5d83370fb365e3c3eb460ac81f9713c7bf61bafb
Tanya <tanyarajhans7@gmail.com>
2022-02-10 13:56:16 +0530

    New badge added

--- lib/screens/feeds.dart
+++ lib/screens/feeds.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:shopifie/widget/feeds_products.dart';
 
 class Feeds extends StatelessWidget {
@@ -6,7 +7,24 @@ class Feeds extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: FeedProducts(),)
+      body: GridView.count(
+          crossAxisCount: 2,
+          childAspectRatio: 240 / 290,
+          crossAxisSpacing: 8,
+          mainAxisSpacing: 8,
+          children: List.generate(100, (index) {
+            return FeedProducts();
+          }),
+        ),
     );
   }
 }
+
+// GridView.count(
+//         crossAxisCount: 2,
+//         childAspectRatio: 250/300,
+//         crossAxisSpacing: 8,
+//         mainAxisSpacing: 8,
+//         children: 
+//           List.generate(100, (index) => FeedProducts())
+//         ,)
--- lib/widget/feeds_products.dart
+++ lib/widget/feeds_products.dart
@@ -1,3 +1,4 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 
 class FeedProducts extends StatefulWidget {
@@ -10,75 +11,95 @@ class FeedProducts extends StatefulWidget {
 class _FeedProductsState extends State<FeedProducts> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 250,
-      height: 290,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(6),
-        color: Theme.of(context).backgroundColor,
-      ),
-      child: Column(children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(2),
-          child: Container(
-            width: double.infinity,
-            constraints: BoxConstraints(
-              minHeight: 100,
-              maxHeight: MediaQuery.of(context).size.height*0.3,
-              
-            ),
-            child: Image.network('https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg',
-            fit: BoxFit.fitWidth,
-            height: MediaQuery.of(context).size.height*0.2,),
-          )
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Container(
+        width: 250,
+        height: 300,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6),
+          color: Theme.of(context).backgroundColor,
         ),
-        Container(
-          padding: EdgeInsets.only(left: 5.0),
-          margin: EdgeInsets.only(left: 5.0, right: 3.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+        child: Column(children: [
+          Stack(
             children: [
-              SizedBox(height: 4),
-              Text('Description',
-                overflow: TextOverflow.ellipsis,
-                maxLines: 2,
-                style: TextStyle(
-                  fontSize: 15,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w600
-                ),),
-                Text('\$ 179.99',
-                overflow: TextOverflow.ellipsis,
-                maxLines: 2,
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w900
-                ),),
-                Row(children: [
-                  Text('Quantity: 12',
-                overflow: TextOverflow.ellipsis,
-                maxLines: 2,
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Colors.grey,
-                  fontWeight: FontWeight.w600
-                ),),
-                Material(
-                  color: Colors.transparent,
-                  child: InkWell(
-                    onTap:(){},
-                    borderRadius: BorderRadius.circular(18.0),
-                    child: Icon(Icons.add,
-                    color: Colors.grey),
+              
+              ClipRRect(
+              borderRadius: BorderRadius.circular(2),
+              child: Container(
+                width: double.infinity,
+                constraints: BoxConstraints(
+                  minHeight: 100,
+                  maxHeight: MediaQuery.of(context).size.height*0.3,
+                  
+                ),
+                child: Image.network('https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg',
+                fit: BoxFit.fitWidth,
+                height: MediaQuery.of(context).size.height*0.15,),
+              )
+            ),
+            Badge(
+      toAnimate: true,
+      shape: BadgeShape.square,
+      badgeColor: Colors.pink,
+      borderRadius: BorderRadius.only(bottomRight: Radius.circular(8.0)),
+      badgeContent: Text('New', style: TextStyle(color: Colors.white)),
+    ),
+            ]
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 5.0),
+            margin: EdgeInsets.only(left: 5.0, right: 3.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 4),
+                Text('Description',
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600
+                  ),),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical:8.0),
+                    child: Text('\$ 179.99',
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w900
+                    ),),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                    Text('Quantity: 12',
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w600
+                  ),),
+                  Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      onTap:(){},
+                      borderRadius: BorderRadius.circular(18.0),
+                      child: Icon(Icons.add,
+                      color: Colors.grey),
 
+                    )
                   )
-                )
-                ],)
-            ],
+                  ],)
+              ],
+            )
           )
-        )
-      ],)
+        ],)
+      ),
     );
   }
 }
