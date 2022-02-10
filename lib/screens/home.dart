d58e3b5da41813b2bef6d2395cec1108ef44b5ba
Tanya <tanyarajhans7@gmail.com>
2022-02-10 20:31:27 +0530

    Carousel added

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,28 @@
 import 'package:backdrop/backdrop.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 import '../consts/colors.dart';
 
-class Home extends StatelessWidget {
+
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+final List<String> imgList = [
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
 
   @override
   Widget build(BuildContext context) {
@@ -42,9 +61,20 @@ class Home extends StatelessWidget {
         backLayer: const Center(
           child: Text("Back Layer"),
         ),
-        frontLayer: const Center(
-          child: Text("Front Layer"),
-        ),
+        frontLayer: Container(
+          height:190,
+          width: double.infinity,
+          child: CarouselSlider(
+          options: CarouselOptions(),
+          items: imgList
+              .map((item) => Container(
+                    child: Center(
+                        child:
+                            Image.network(item, fit: BoxFit.cover, width: 1000)),
+                  ))
+              .toList(),
+      ),
+        )
       ),
       
     );
