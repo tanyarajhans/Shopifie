ad01888b1326b2a51ee3d8f165da6f3d5bb04d31
Tanya <tanyarajhans7@gmail.com>
2022-02-20 14:45:07 +0530

    Product details screen added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shopifie/consts/theme_data.dart';
 import 'package:shopifie/inner_screens/brands_navigation_rail.dart';
+import 'package:shopifie/inner_screens/product_details.dart';
 import 'package:shopifie/provider/dark_theme_provider.dart';
 import 'package:shopifie/screens/bottom_bar.dart';
 import 'package:shopifie/screens/cart.dart';
@@ -52,6 +53,7 @@ class _MyAppState extends State<MyApp> {
           Cart.routeName : (context) => Cart(),
           Feeds.routeName : (context) => Feeds(),
           Wishlist.routeName : (context) => Wishlist(),
+          ProductDetails.routeName :(context) => ProductDetails()
         },
       );
       }
