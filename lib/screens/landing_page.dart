f7ca06c5b709d3bd2fb652da9cb1ab647f361aee
Tanya <tanyarajhans7@gmail.com>
2022-04-09 00:21:46 +0530

    landing page part 2 done

--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:provider/provider.dart';
 import 'package:shopifie/consts/colors.dart';
 
 class LandingPage extends StatefulWidget {
@@ -10,20 +11,46 @@ class LandingPage extends StatefulWidget {
   State<LandingPage> createState() => _LandingPageState();
 }
 
-class _LandingPageState extends State<LandingPage> {
+class _LandingPageState extends State<LandingPage> with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
   @override
+
+  void initState(){
+    super.initState();
+    _animationController=AnimationController(vsync: this, duration: Duration(seconds: 20));
+    _animation=CurvedAnimation(parent: _animationController, curve: Curves.linear)
+    ..addListener((){
+      setState(() {
+        
+      });
+    })..addStatusListener((animationStatus){
+      if(animationStatus==AnimationStatus.completed){
+        _animationController.reset();
+      }
+    });
+    _animationController.forward();
+  }
+
+  @override
+  void dispose(){
+    _animationController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           CachedNetworkImage(
-            imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaeYSXISe0Unq2mDJIMAjnTphT5AREWym9Pw&usqp=CAU',
-            placeholder: (context, url)=>Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0AVvdQVH2LdtsYFauXEnnrgt2CG1diwowkJ1qLwGXLjJrzStCxFnH4p2rEDA9jkomk54&usqp=CAU',
-            fit: BoxFit.contain),
+            imageUrl: 'https://st3.depositphotos.com/7375876/i/600/depositphotos_318573922-stock-photo-shopping-bags-hands-hand-young.jpg',
+         //  placeholder: (context, url)=>Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0AVvdQVH2LdtsYFauXEnnrgt2CG1diwowkJ1qLwGXLjJrzStCxFnH4p2rEDA9jkomk54&usqp=CAU',
+          // fit: BoxFit.contain),
             errorWidget: (context, url, error)=>Icon(Icons.error_outline),
-            fit: BoxFit.contain,
+            fit: BoxFit.cover,
             height: double.infinity,
             width: double.infinity,
+            alignment: FractionalOffset(_animation.value, 0),
           ),
           Container(
             margin: EdgeInsets.only(top: 30),
@@ -71,11 +98,19 @@ class _LandingPageState extends State<LandingPage> {
                     )),
                     
                       onPressed: (){}, 
-                      child: Text('Login',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 17
-                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                        Text('Login',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 17
+                        ),
+                        ),
+                        SizedBox(width: 5,),
+                        Icon(Icons.supervised_user_circle,
+                        size: 20,)
+                        ]
                       )
                     ),
                   ),
@@ -92,11 +127,19 @@ class _LandingPageState extends State<LandingPage> {
                       ),
                     )),
                       onPressed: (){}, 
-                      child: Text('Sign Up',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 17
-                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                        Text('Sign Up',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 17
+                        ),
+                        ),
+                        SizedBox(width: 5,),
+                        Icon(Icons.verified_user_outlined,
+                        size: 20,)
+                        ]
                       )
                     ),
                   ),
@@ -126,7 +169,26 @@ class _LandingPageState extends State<LandingPage> {
                     ),
                   ),
                 ],
-              )
+              ),
+              SizedBox(height: 30,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  OutlineButton(onPressed: (){}, 
+                  shape: StadiumBorder(),
+                  highlightedBorderColor: Colors.red.shade200,
+                  borderSide: BorderSide(width: 2, color: Colors.red ),
+                  child: Text('Google +')
+                  ),
+                  OutlineButton(onPressed: (){}, 
+                  shape: StadiumBorder(),
+                  highlightedBorderColor: Colors.deepPurple.shade200,
+                  borderSide: BorderSide(width: 2, color: Colors.deepPurple ),
+                  child: Text('Sign In as a Guest')
+                  ),
+                ],
+              ),
+              SizedBox(height: 40,),
             ],
           )
         ],
